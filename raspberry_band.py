8ea3cc0566f6dc61613406235cdcd0115d3e0c95
stevewmli <steve.wmli@gmail.com>
2020-12-19 22:51:20 -0800

    update the main python script

--- raspberry_band.py
+++ raspberry_band.py
@@ -13,34 +13,84 @@ except ModuleNotFoundError:
 
 import tqdm
 
-class RaspberryBand:
-    def init(
-        self,
-        servo_1_centre=1500,
-        servo_2_centre=1500
-    ):
-        self.servo_1_centre = servo_1_centre
-        self.servo_2_centre = servo_2_centre
+class ServoConfig:
+    def init(self, center_pw, center_angle, range, angle_pws, degree_ms):
+        self.center_pw = center_pw
+        self.center_angle = center_angle
+        self.range = range
+        self.angle_pws = angle_pws
+        self.degree_ms = degree_ms
 
+
+class Player:
+    def init(self, rpi, num_of_notes, turner, drummer, turner_config, drummer_config):
+        self.rpi = rpi
+        self.num_of_notes = num_of_notes
+        self.angle_btw_notes = (turner_config.range[1] - turner_config.range[0]) / (num_of_notes - 1)
+        self.turner = turner
+        self.drummer = drummer
+        self.turner_config = turner_config
+        self.drummer_config = drummer_config
+
+        # the pulse frequency should be no higher than 100Hz - higher values could (supposedly) damage the servos
+        self.rpi.set_PWM_frequency(turner, 50)
+        self.rpi.set_PWM_frequency(drummer, 50)
+
+        # initialize servo position
+        self.rpi.set_servo_pulsewidth(turner, self.angles_to_pw(turner_config, 90))
+        self.rpi.set_servo_pulsewidth(drummer, self.angles_to_pw(drummer_config, 90))
+
+    def angles_to_pw(self, servo_config, angle):
+        if servo_config.angle_pws:
+            servo_array = numpy.array(angle_pws)
+            return numpy.poly1d(
+                numpy.polyfit(
+                    servo_array[:,0],
+                    servo_array[:,1],
+                    3
+                )
+            )
+
+        else:
+            return self.naive_angles_to_pulse_widths
+
+    def naive_angles_to_pulse_widths(self, servo_config, angle):
+        return (angle - servo_config.center_angle) * servo_config.degree_ms + servo_config.centre
+        
+
+    def play_note(self, note_id):
+        # turn to the correct note
+        start_angle = self.turner_config.range[0]
+        target_angle = start_angle + angle_btw_notes * note_id
+        
+        self.angles_to_pw(turner_config, target_angle)
+
+        # hit the note
+        self.angles_to_pw(drummer_config, 105)
+        self.angles_to_pw(drummer_config, 90)
+
+class RaspberryBand:
+    def init(self):
         # instantiate this Raspberry Pi as a pigpio.pi() instance
         self.rpi = pigpio.pi()
 
-        # the pulse frequency should be no higher than 100Hz - higher values could (supposedly) damage the servos
-        self.rpi.set_PWM_frequency(14, 50)
-        self.rpi.set_PWM_frequency(15, 50)
-
-        # Initialise the pantograph with the motors in the centre of their travel
-        self.rpi.set_servo_pulsewidth(14, self.angles_to_pw_1(90))
-        sleep(0.3)
-        self.rpi.set_servo_pulsewidth(15, self.angles_to_pw_2(90))
-        sleep(0.3)
+        self.players[0] = Player(self.rpi, 7, 2, 3, 
+                                 ServoConfig(1500, 90, [30.0, 150.0], [], 10), 
+                                 ServoConfig(1500, 90, [30.0, 150.0], [], 10))
 
         # by default we use a wait factor of 0.1 for accuracy
         self.wait = wait or .1
 
 
 def main():
-    print("Hello World")
+    band = RaspberryBand()
+    band.players[0].play_note(0)
+    band.players[0].play_note(1)
+    band.players[0].play_note(2)
+    band.players[0].play_note(3)
+    band.players[0].play_note(4)
+    band.players[0].play_note(5)
+    band.players[0].play_note(6)
 
 if __name__ == "__main__":
     main()
